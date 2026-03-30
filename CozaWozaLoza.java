aa9d35b00f86e397e6290ad77982080cf30713d2
Fidel Castro <fidelcastro4080@gmail.com>
2026-03-30 18:08:43 +0300

    coza woza loza

---
+++ CozaWozaLoza.java
@@ -0,0 +1,28 @@
+public class CozaWozaLoza {
+    public static void main(String[] arg){
+        for(int i = 1; i <= 110; i++){
+
+            if(i % 3 == 0){
+                System.out.print("Coza");
+            }
+
+            if(i % 5 == 0){
+                System.out.print("Loza");
+            }
+
+            if(i % 7 == 0){
+                System.out.print("Woza");
+            }
+
+            if(i % 3 != 0 && i % 5 != 0 && i % 7 != 0){
+                System.out.print(i);
+            }
+
+            System.out.print(" ");
+
+            if(i % 11 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
