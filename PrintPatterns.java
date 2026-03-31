066f31dbd5baf8267c33f9623b4d950f318682c7
Fidel Castro <fidelcastro4080@gmail.com>
2026-03-31 16:27:47 +0300

    patterns

--- PrintPatterns.java
+++ PrintPatterns.java
@@ -4,15 +4,49 @@ public class PrintPatterns {
 
             for(int cols = 1; cols <= rows; cols++){
 
-                System.out.print(" #");
+                System.out.print("# ");
             }
 
             System.out.println();
         }
     }
 
+    static void pattern2(){
+        for(int rows = 1; rows <= 8; rows++){
+
+            for(int cols = 8; cols >= rows; cols--){
+                System.out.print("# ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern3(){
+        for(int rows = 1; rows <= 8; rows++){
+
+            for(int spaces = 1; spaces <= (rows -1); spaces++){
+                System.out.print("  ");
+            }
+
+            for(int cols = 8; cols >= rows; cols--){
+                System.out.print("# ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern4(){
+
+    }
+
     public static void main(String[] args){
-        pattern1();
+        // pattern1();
+        // pattern2();
+        // pattern3();
+        pattern4();
+
     }
 }
 
